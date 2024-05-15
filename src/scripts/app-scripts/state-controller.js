273b8c5e52a2f0b9989460806a3daa5590a67035
okediarthur <okediarthur@gmail.com>
2024-05-15 22:41:15 +0300

    Events and State Controllers added

---
+++ src/scripts/app-scripts/state-controller.js
@@ -0,0 +1,93 @@
+import { getState } from "./state";
+import { updateStorage } from "./storage";
+
+function getTasks(projectIndex){
+    return getState().projects[projectIndex].tasks
+}
+
+//Project Methods
+const project = {
+    create(name){
+        return {
+            name,
+            selected: false,
+            tasks:[],
+        };
+    },
+    add(name) {
+        getState().projects.push(this.create(name));
+        updateStorage(getState());
+    },
+    rename(projectIndex, name){
+        getState().projects[projectIndex].name = name;
+        updateStorage(getState());
+    },
+    remove(projectIndex){
+        getState().projects.splice(projectIndex, 1);
+
+        if(projectIndex === this.getSelectedIndex()) {
+            this.setSelected(null);
+            return;
+        }
+
+        updateStorage(getState());
+    },
+    getSelected(){
+        const selectedProject = getState().projects.find(project => project.selected);
+
+        return selectedProject ? selectedProject : null;
+    },
+    getSelectedIndex(){
+        return getState().selectedProjectIndex;
+    },
+    setSelected(projectIndex){
+        const alreadySelected = this.getSelected();
+        if(alreadySelected) {
+            alreadySelected.selected = false;
+        }
+        if(projectIndex !== null){
+            getState().projects[projectIndex].selected = true;
+        }
+        getState().selectedProjectIndex = projectIndex;
+
+        updateStorage(getState());
+    },
+    get(projectIndex){
+        return getState().projectss[projectIndex];
+    }
+};
+
+// Task Methods
+const task = {
+    create(title, description, dueDate, priority, completed=false){
+        return {
+            title,
+            description,
+            dueDate,
+            priority,
+            completed,
+        };
+    },
+    add(projectIndex, ...taskProperties){
+        getTasks(projectIndex).push(this.create(...taskProperties));
+        updateStorage(getState());
+    },
+    replace(projectIndex, taskIndex, ...newTaskProperties){
+        getTasks(projectIndex)[taskIndex] = this.create(...newTaskProperties);
+        updateStorage(getState());
+    },
+    switchCompleted(projectIndex, taskIndex){
+        const task = getTasks(projectIndex)[taskIndex];
+        task.completed = task.completed ? false : true;
+        updateStorage(getState());
+    },
+    remove(projectIndex, taskIndex){
+        getTasks(projectIndex).splice(taskIndex, 1);
+        updateStorage(getState());
+    },
+    get(projectIndex, taskIndex){
+        getTasks(projectIndex)[taskIndex];
+    },
+};
+
+export { project, task };
